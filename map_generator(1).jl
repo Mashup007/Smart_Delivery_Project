d94876dd82e1f46370dd39876565074e0c72b747
Mashup007 <37002449+Mashup007@users.noreply.github.com>
2018-08-03 15:49:26 -0500

    adsf

---
+++ map_generator(1).jl
@@ -0,0 +1,67 @@
+seed = srand(124534)
+
+
+# Parameters
+latitude = 10
+longtitude = 10
+density = 0.1
+time_length = 1000
+max_tip = 10
+# ------------
+
+vertical = 1
+horizontal = 2
+
+c = zeros(latitude, longtitude)  #whether a node is a costomer node
+time = zeros(latitude, longtitude)
+cost = 2*time_length*ones(latitude, longtitude, 2)
+#cost[i][j][vertical] --> the cost from (i, j) to (i+1, j)
+#cost[i][j][horizontal] --> the cost from (i, j) to (i, j+1)
+
+tips = zeros(latitude, longtitude)
+
+for i in 1:latitude
+    for j in 1:longtitude
+        if rand() < density  #generate a customer;
+            c[i, j] = 1;
+            time[i, j] = rand() * time_length;
+            tips[i, j] = rand() * max_tip;
+        end
+
+        if i != latitude #generate the cost of each legal route
+            cost[i, j, vertical] = ceil(Int64, rand() * time_length/(latitude*longtitude)*((1-density).^2))
+        end
+        if j != longtitude
+            cost[i, j, horizontal] = ceil(Int64, rand() * time_length/(latitude*longtitude)*((1-density).^2))
+        end
+    end
+end
+
+c[ceil(Int64, rand() * latitude), ceil(Int64, rand() * longtitude)] = 2
+
+
+@printf("cost graph of the map:")
+
+for i in 1:latitude
+    if i != 1
+        for j in 1:longtitude
+            @printf("%2.d      ", cost[i-1, j, vertical])
+        end
+    end
+    @printf("\n")
+    for j in 1:longtitude
+        if c[i, j] == 1
+            @printf(" C  ")
+        end
+        if c[i, j] == 2
+            @printf(" R  ")
+        end
+        if c[i, j] == 0
+            @printf(" 0  ")
+        end
+        if j != longtitude
+            @printf("%2.d  ", cost[i, j, horizontal]);
+        end
+    end
+    @printf("\n")
+end
