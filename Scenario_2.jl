ad854c42217304976a04149d7cb6eca35fb773e7
Mashup007 <37002449+Mashup007@users.noreply.github.com>
2018-08-10 12:14:54 -0500

    asdf

--- Scenario_2.jl
+++ Scenario_2.jl
@@ -40,7 +40,6 @@ for i in 1:length(Routines) # largest Array
         exceed_capacity = 0
             for a in 1:length(Routines[i][k]) # lowest Array
                 sum += Customers_Request[Routines[i][k][a]]
-                #println([Routines[i][k][a]])
                 if sum > Vehicle_Max_Capacity
                     sum -= Customers_Request[Routines[i][k][a]]
                     exceed_capacity = 1 #indicate that this set exceeded max capacity
@@ -56,7 +55,6 @@ end
 println("Total elements should be in Actual_Routines: ", x )
 println("Actual_Routines_length: ",length(Actual_Routines))
 println("Actual_Routines: ",Actual_Routines)
-# println(length(Actual_Routines[end]))
 
 for i in 1:length(Actual_Routines)
     cost = 0
@@ -79,32 +77,15 @@ end
 println(Cost_Array)
 println("Cost_Arry length: ", length(Cost_Array))
 
-################## Coverage_Array #################################33333
-# coverage_counter = 0
-#
-# for k in 2:length(Customers) # 1
-#     #customer_counter = 0
-#     for i in 1:length(Actual_Routines) # go through all routines
-#         for j in 1:length(Actual_Routines[i]) # go through a specific routine
-#             if Actual_Routines[i][j] == Customers[k]
-#                 push!(Coverage_Array, Actual_Routines[i])
-#                 coverage_counter += 1 #coverage_counter == how many routines are covering THIS customer
-#             end
-#         end
-#     end
-#     push!(Cover_Counter_Array, coverage_counter)
-# end
-# println(Cover_Counter_Array)
-# println("length of coverage array: ", length(Coverage_Array))
-# println("Coverage_Array: ", Coverage_Array)
+
 
 Solve_Array = Int64[]
 for i in 1:length(Actual_Routines)
     push!(Solve_Array, i)
 end
 println("Solve Array:", Solve_Array)
-########################################################################
 
+####################### Solving The Problem ###################################
 @variable(m, z[Solve_Array], Bin)
 
 support_array = Int64[] # wipe out everything unecessary
@@ -162,7 +143,3 @@ end
 solve(m)
 println(getobjectivevalue(m))
 println(getvalue(z))
-
- println(Actual_Routines[16])
- println(Actual_Routines[2])
- println(Actual_Routines[3])
