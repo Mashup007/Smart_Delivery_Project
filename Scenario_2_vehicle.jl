d7e346394b49904a7528f6661faca8913766b5fc
Mashup007 <37002449+Mashup007@users.noreply.github.com>
2018-08-10 14:39:31 -0500

    a

--- Scenario_2_vehicle.jl
+++ Scenario_2_vehicle.jl
@@ -2,7 +2,7 @@ using JuMP, Gurobi, NamedArrays
 
 m = Model(solver = GurobiSolver(OutputFlag=0))
 
-Routine = [:1 :2 :3 :4 :5 :6 :7 :8 :9]
+Routine = [:1 :2 :3 :4 :5 :6 ]
 # We have k vehicles
 Vehicle_Max_Capacity = 30 # max capacity of each Truck
 #---------------------------------------------------------------
@@ -10,16 +10,16 @@ Vehicle_Max_Capacity = 30 # max capacity of each Truck
 Cost_Per_Mile = 2 # cost of gas per mile for Vehicles
 
 #Profit/unit = 20 for everything. SO --> Min Cost == Max Profit
-Customers = [1,2,3,4,5,6] # 1 is resturant. Total Customers == 5.
+Customers = [1,2,3,4] # 1 is resturant. Total Customers == 5.
 Customers_Request = Dict(zip(Customers, [0 11 9 8 12 14]))
 Pure_Profit = Dict(zip(Customers,[0 220 180 160 240 280]))
 
-Distances_D_M = NamedArray([0 18 14 16 12 19
-                            18 0 18 30 26 28
-                            14 18 0 20 22 21
-                            16 30 20 0 22 21
-                            12 26 22 22 0 22
-                            19 28 21 21 22 0],(Customers,Customers),("Customers","Customers"))
+# Distances_D_M = NamedArray([0 18 14 16 12 19
+#                             18 0 18 30 26 28
+#                             14 18 0 20 22 21
+#                             16 30 20 0 22 21
+#                             12 26 22 22 0 22
+#                             19 28 21 21 22 0],(Customers,Customers),("Customers","Customers"))
 println("[1,1] is restaurant")
 println(Distances_D_M)
 println("***Rows and Columns represent the distance between Customers [reminder: 1,1 is restaurant]")
@@ -37,18 +37,17 @@ println("***Rows and Columns represent the distance between Customers [reminder:
 # z[1] = 1-2-3-4
 # z[2] = 1-2-4
 # z[3] = 1-2-5
-# z[4] = 1-2-6
-# z[5] = 1-3-4-5
+# z[4] = 1-2-6   0 - 1 -5
+# z[5] = 1-3-4-5 0 - 2-3-4
 # z[6] = 1-3-4
 # z[7] = 1-4-5
 # z[8] = 1-4-6
 # z[9] = 1-5-6
 
-@constraint(m, z[1]+z[2]+z[3]+z[4] >= 1) # 2 is covered
-@constraint(m, z[1]+z[5]+z[6] >= 1) # 3 is covered
-@constraint(m, z[2]+z[5]+z[6]+z[7]+z[8] >= 1) # 4 is covered
-@constraint(m, z[3]+z[5]+z[7]+z[9] >= 1) # 5 is covered
-@constraint(m, z[4]+z[8]+z[9] >= 1) # 6 is covered
+@constraint(m, z[1]+z[2]+z[4]+z[5] >= 1) # 2 is covered
+@constraint(m, z[1]+z[3]+z[4]+z[6] >= 1) # 3 is covered
+@constraint(m, z[2]+z[3]+z[5]+z[6] >= 1) # 4 is covered
+
 
 @objective(m, Min, sum(z[j] for j in Routine))
 
